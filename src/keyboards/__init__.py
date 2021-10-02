16e1ff2a3eac3594f51f57d04914f8485627a5b1
onixuniverse <warofmyhome@gmail.com>
2021-10-02 11:05:24 +0500

    изменена регистрация пользователей

--- src/keyboards/__init__.py
+++ src/keyboards/__init__.py
@@ -1,4 +1,4 @@
-default = open(r"src/keyboards/default.json", 'r', encoding="utf-8").read()
+main_menu = open(r"src/keyboards/default.json", 'r', encoding="utf-8").read()
 help_page = open(r"src/keyboards/help_page.json", 'r', encoding="utf-8").read()
 to_main_page = open(r"src/keyboards/to_main_page.json", 'r', encoding="utf-8").read()
 ready = open(r"src/keyboards/ready.json", 'r', encoding="utf-8").read()
